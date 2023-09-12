2753eb043d06cd71bc0fa0eadab48b285c1ddceb
AnoushkaDhar <128288400+anoushkadhar123@users.noreply.github.com>
2023-09-12 14:27:48 +0530

    index.js

--- index.js
+++ index.js
@@ -1,130 +1,109 @@
 const buttons = document.querySelectorAll("button");
 const resultEl = document.getElementById("result");
-const resultE2 = document.getElementById("statistics");
 const playerScoreEl = document.getElementById("user-score");
 const computerScoreEl = document.getElementById("computer-score");
-
-const leaderboard = document.getElementById("leaderboard");
-
-function updateLeaderboard(winner, icon, time) {
-  const listItem = document.createElement("li");
-  listItem.innerHTML = `${icon} ${winner} - ${time}`;
-  leaderboard.appendChild(listItem);
-}
-
+const victory = document.getElementById("victory");
 let playerScore = 0;
 let computerScore = 0;
-
 let time;
 let winner;
-let winnerIcon;
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const result = playRound(button.id, computerPlay());
-    const result1 = score(button.id, computerPlay());
-    resultE2.textContent = result1;
-    resultEl.textContent = result;
-  });
-});
-
-function computerPlay() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
-}
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    time = new Date().toLocaleTimeString();
-    return "It's a Tie!";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    time = new Date().toLocaleTimeString();
-    winner = "Player";
-    playerScore++;
-    playerScoreEl.textContent = playerScore;
-    winnerIcon = "&#x1F44A;";
-    updateLeaderboard();
-    return "You Win! " + playerSelection + " beats " + computerSelection;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    time = new Date().toLocaleTimeString();
-    winner = "Player";
-    playerScore++;
-    playerScoreEl.textContent = playerScore;
-    winnerIcon = "&#x1f590";
-    updateLeaderboard();
-    return "You Win! " + playerSelection + " beats " + computerSelection;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    time = new Date().toLocaleTimeString();
-    winner = "Player";
-    playerScore++;
-    playerScoreEl.textContent = playerScore;
-    winnerIcon = "&#x270c;";
-    updateLeaderboard();
-    return "You Win! " + playerSelection + " beats " + computerSelection;
-  } else {
-    time = new Date().toLocaleTimeString();
-    winner = "Computer";
-    computerScore++;
-    computerScoreEl.textContent = computerScore;
-    winnerIcon = "&#x1f590;";
-    updateLeaderboard();
-    return "You Lose! " + computerSelection + " beats " + playerSelection;
-  }
-}
+let winnericon;
+const leaderboard = document.getElementById("leaderboard");
 
 function updateLeaderboard() {
-  const leaderboardEl = document.getElementById("leaderboard");
-  const entryEl = document.createElement("div");
-  entryEl.innerHTML = `${time} ${winner} wins by ${winnerIcon}`;
-  leaderboardEl.appendChild(entryEl);
-}
-
-
-
-let win = "";
-let lose = "";
-function score(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    win = "";
-    lose = "";
-    return "It's a tie!";
-  }
-  else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")) {
-    playerScore++;
-    playerScoreEl.textContent = playerScore;
-    win = "You";
-    lose = "Computer";
-    return " You win! " + playerSelection + " beats " + computerSelection;
-  }
-  else {
-    computerScore++;
-    computerScoreEl.textContent = computerScore;
-    win = "Computer";
-    lose = "You";
-    return " You lose! " + computerSelection + " beats " + playerSelection;
-  }
-}
-function updateScores() {
-  playerScoreEl.textContent = playerScore;
-  computerScoreEl.textContent = computerScore;
-}
-function updateResultText(result) {
-  if (win !== "" && lose !== "") {
-    resultEl.textContent = win + " wins! " + win + " scored " + playerScore + " points and " + lose + " scored " + computerScore + " points.";
-  }
-  else {
-    resultEl.textContent = result;
-  }
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${time} ${winner} wins by ${winnericon}`;
+    leaderboard.appendChild(listItem);
+    if(playerScore === computerScore){
+        victory.textContent = "It's a Tie";
+    }
+    else if(playerScore > computerScore){
+        victory.textContent = "USER  (🏆)";
+    }
+    else{
+        victory.textContent = "Computer (🏆)" ;
+    }
 }
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const result = playRound(button.id, computerPlay());
-    updateScores();
-    updateResultText(result);
-  });
+    button.addEventListener("click", () => {
+        const result = playRound(button.id, computerPlay());
+        resultEl.textContent = result;
+    });
 });
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        time = getCurrentTime();
+        return "It's a tie!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        time = getCurrentTime();
+        winner = "User";
+        playerScore++;
+        playerScoreEl.textContent = playerScore;
+        winnericon = "&#x1F44A;";
+        updateLeaderboard();
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        time = getCurrentTime();
+        winner = "User";
+        playerScore++;
+        playerScoreEl.textContent = playerScore;
+        winnericon = "&#x1f590;";
+        updateLeaderboard();
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        time = getCurrentTime();
+        winner = "User";
+        playerScore++;
+        playerScoreEl.textContent = playerScore;
+        winnericon = "&#x270c;";
+        updateLeaderboard();
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        time = getCurrentTime();
+        winner = "Computer"
+        computerScore++;
+        computerScoreEl.textContent = computerScore;
+        winnericon = "&#x1F44A;";
+        updateLeaderboard();
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        time = getCurrentTime();
+        winner = "Computer"
+        computerScore++;
+        computerScoreEl.textContent = computerScore;
+        winnericon = "&#x1f590;";
+        updateLeaderboard();
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        time = getCurrentTime();
+        winner = "Computer"
+        computerScore++;
+        computerScoreEl.textContent = computerScore;
+        winnericon = "&#x270c;";
+        updateLeaderboard();
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
 
+function getCurrentTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    if (hours > 12) {
+        return `${hours - 12}:${minutes} PM`;
+    }
+    else {
+        return `${hours}:${minutes} AM`;
+    }
+} 
